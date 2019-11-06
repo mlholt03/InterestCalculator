196d3064bb20272a768b2c6b1d913dc9e7840f77
Matthew Holthouser <matt.holthouser@gmail.com>
2019-11-05 19:33:53 -0500

    Code cleanup

--- Src/Interfaces/IInterestCalculator.cs
+++ Src/Interfaces/IInterestCalculator.cs
@@ -7,10 +7,5 @@ namespace InterestCalculator.Src.Interfaces
     public interface IInterestCalculator<T> where T : IInterestBearing
     {
         void CalculateInterest(T interestBearing);
-        //Person Updater --> Wallet Updater
-        //Wallet Updater --> Line Updater
-        //Line Updater
-
-        //InterestBearingOutputer --> Accepts object of type InterestBearing -> Returns interest amount
     }
 }
